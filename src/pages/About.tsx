cff77d0efb3742065ecaf585cbfeefea2fea684e
Artemiusz Kuzniecow <arty.kuznetsov@yahoo.com>
2023-01-06 21:52:32 +0300

    fixed some bugs

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -43,7 +43,7 @@ const About = () => {
           className="drop-shadow-2xl rounded-md"
         />
         <div className="w-full text-center">
-          <Link to="/contact">
+          <Link to="/contacts">
             <button
               type="button"
               className="inline-block px-6 py-2 border-2 border-blue-400 text-blue-400 font-medium text-xs leading-tight uppercase rounded-full hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out"
