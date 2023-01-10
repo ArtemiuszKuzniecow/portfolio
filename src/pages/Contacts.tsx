6e92c6d074bb716d7b0a3a73b97bce86f66e2af6
Artemiusz Kuzniecow <arty.kuznetsov@yahoo.com>
2023-01-10 17:29:17 +0300

    added some components

--- src/pages/Contacts.tsx
+++ src/pages/Contacts.tsx
@@ -1,7 +1,37 @@
 import * as React from "react";
+import { useSelector } from "react-redux";
+import HeadlineMain from "../components/common/Headlines/HeadlineMain";
+import content from "../content.json";
+import { getLanguageSelector } from "../store/selectors";
 
 const Contacts = () => {
-  return <h1>Contacts page</h1>;
+  const language = useSelector(getLanguageSelector());
+  return (
+    <>
+      <HeadlineMain>
+        {content[language as keyof typeof content].navbarMenu[2]}
+      </HeadlineMain>
+
+      <div className="m-10 p-4 shadow-[0_3px_10px_rgb(0,0,0,0.2)] p-5 box-border">
+        <ul>
+          <li>{content[language as keyof typeof content].contacts.location}</li>
+          <li>
+            <div
+              dangerouslySetInnerHTML={{
+                __html:
+                  content[language as keyof typeof content].contacts.email,
+              }}
+              className="rounded-md"
+            />
+          </li>
+          <li>{content[language as keyof typeof content].contacts.phone}</li>
+          <li>{content[language as keyof typeof content].contacts.telegram}</li>
+          <li>{content[language as keyof typeof content].contacts.whatsapp}</li>
+          <li>{content[language as keyof typeof content].contacts.viber}</li>
+        </ul>
+      </div>
+    </>
+  );
 };
 
 export default Contacts;
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,7 +1,16 @@
 import * as React from "react";
+import HeadlineMain from "../components/common/Headlines/HeadlineMain";
+import content from "../content.json";
+import { getLanguageSelector } from "../store/selectors";
+import { useSelector } from "react-redux";
 
 const Main = () => {
-  return <h1>Main page</h1>;
+  const language = useSelector(getLanguageSelector());
+  return (
+    <HeadlineMain>
+      {content[language as keyof typeof content].navbarMenu[3]}
+    </HeadlineMain>
+  );
 };
 
 export default Main;
