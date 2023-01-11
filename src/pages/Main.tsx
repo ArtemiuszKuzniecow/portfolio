ee40edd51181c4ec5808d2c167fe65b34dfbb808
Artemiusz Kuzniecow <arty.kuznetsov@yahoo.com>
2023-01-11 13:20:45 +0300

    finished work with that website

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -4,22 +4,30 @@ import content from "../content.json";
 import { getLanguageSelector } from "../store/selectors";
 import { useSelector } from "react-redux";
 import HeadlineSecond from "../components/common/Headlines/HeadlineSecond";
+import MainImage from "../assets/MainImage";
 
 const Main = () => {
   const language = useSelector(getLanguageSelector());
   return (
     <>
       <div className="container flex lg:justify-around max-lg:justify-evenly p-10 flex-wrap items-center content-center anim ">
-        <div>
-          <HeadlineMain>
-            {content[language as keyof typeof content].navbarMenu[3]}
-          </HeadlineMain>
-          <HeadlineSecond>
-            {content[language as keyof typeof content].name.toUpperCase()}{" "}
-            {content[language as keyof typeof content].profession.toUpperCase()}
-          </HeadlineSecond>
+        <HeadlineSecond>
+          {content[language as keyof typeof content].name.toUpperCase()}{" "}
+          {content[language as keyof typeof content].profession.toUpperCase()}
+        </HeadlineSecond>
+        <div className="flex flex-wrap justify-around">
+          <div className="max-sm:hidden">
+            <MainImage />
+          </div>
+          <div className="w-1/3 max-xl:w-full">
+            <HeadlineMain>
+              {content[language as keyof typeof content].navbarMenu[3]}
+            </HeadlineMain>
+            <p className="pt-5">
+              {content[language as keyof typeof content].greetings}
+            </p>
+          </div>
         </div>
-        <p>{content[language as keyof typeof content].greetings}</p>
       </div>
     </>
   );
